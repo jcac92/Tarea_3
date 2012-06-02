634f46efdd4ee76a6d7dab5231b891caebc5c26b
Jean Carlo Alfaro <jcac92@hotmail.com>
2012-06-02 13:21:50 -0600

    prueba de lectura por líneas cn loop

--- TP3.lisp
+++ TP3.lisp
@@ -1,6 +1,8 @@
-(let ((in (open "meli.pdf" :if-does-not-exist nil)))
-	(when in
-		(loop for line = (read-line in nil)
-			while line do (format t "~a~%" line))
-		(close in)))
+(defparameter *in* nil)
 
+(setf *in* (open "jcac.pdf" :if-does-not-exist nil))
+
+(when *in*
+	(loop for line = (read-line *in* nil)
+		while line do (format t "~a~%" line))
+	(close *in*))
